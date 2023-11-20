ec3fb015275e7bfe99cf568be330be55bbfeffa9
Ilya_Emelyanenko <91991456+BroKis@users.noreply.github.com>
2023-11-20 10:39:34 +0300

    Update ConfigureDAL.cs

--- ShopGame.DAL/Configuration/DALConfiguration/ConfigureDAL.cs
+++ ShopGame.DAL/Configuration/DALConfiguration/ConfigureDAL.cs
@@ -10,21 +10,15 @@ public static class ConfigurationExtensions
 {
     public static void ConfigureDAL(this IServiceCollection services,string? connection)
     {
-
         services.AddDbContext<ApplicationContext>(option
             =>
         {
             option.UseMySql(connection, new MySqlServerVersion("8.0.32"));
         });
         
-        
         services.AddScoped<IGameRepository, GameRepository>();
         services.AddScoped<IOrderRepository, OrderRepository>();
         services.AddScoped<IGenreRepository, GenreRepository>();
-       
-       
-
-
     }
     
 }
